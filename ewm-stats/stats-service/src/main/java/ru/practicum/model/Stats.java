7369722babf426de7a579c34b0400be7019d204a
Sergey Malyunov <FatalRR@yandex.ru>
2023-06-23 06:21:26 +0700

    с ноута

---
+++ ewm-stats/stats-service/src/main/java/ru/practicum/model/Stats.java
@@ -0,0 +1,27 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "stats")
+public class Stats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String app;
+    private String uri;
+    private String ip;
+    private LocalDateTime timestamp;
+}
