6c1881cba90c533fc98a729ec847a3bec37e579e
Sergey Malyunov <FatalRR@yandex.ru>
2023-06-28 07:26:50 +0700

    feat: change string to LocalDateTime, remove Validated

--- ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/controller/StatsController.java
+++ ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/controller/StatsController.java
@@ -2,8 +2,8 @@ package ru.practicum.stat.service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stat.dto.RequestDto;
 import ru.practicum.stat.dto.ResponseDto;
@@ -11,12 +11,12 @@ import ru.practicum.stat.service.messages.LogMessages;
 import ru.practicum.stat.service.service.StatsService;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Validated
 public class StatsController {
     private final StatsService service;
 
@@ -28,10 +28,10 @@ public class StatsController {
     }
 
     @GetMapping(path = "/stats")
-    public List<ResponseDto> stats(@RequestParam(name = "start") String start,
-                                   @RequestParam(name = "end") String end,
+    public List<ResponseDto> stats(@RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                   @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                    @RequestParam(name = "uris", required = false) List<String> uris,
-                                   @RequestParam(name = "unique", required = false, defaultValue = "false") boolean unique) {
+                                   @RequestParam(name = "unique", defaultValue = "false") boolean unique) {
         log.debug(LogMessages.TRY_GET_STATS.label, start, end, uris, unique);
         return service.stats(start, end, uris, unique);
     }
