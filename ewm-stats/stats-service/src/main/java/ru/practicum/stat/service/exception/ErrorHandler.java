a27d6f5a9a6cd469838c93471337448a87db0c69
Sergey Malyunov <FatalRR@yandex.ru>
2023-06-28 07:26:10 +0700

    feat: add new handler

--- ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/exception/ErrorHandler.java
+++ ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/exception/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.stat.service.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,5 +18,17 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleNotOwnerException(final TimestampException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable throwable) {
+        return new ErrorResponse(throwable.getMessage());
     }
 }
