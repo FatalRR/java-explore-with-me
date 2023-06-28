7001e2be9061268316bd8ebad45d972e6e179f13
Sergey Malyunov <FatalRR@yandex.ru>
2023-06-28 07:27:01 +0700

    feat: change string to LocalDateTime

--- ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/service/StatsService.java
+++ ewm-stats/stats-service/src/main/java/ru/practicum/stat/service/service/StatsService.java
@@ -3,10 +3,11 @@ package ru.practicum.stat.service.service;
 import ru.practicum.stat.dto.RequestDto;
 import ru.practicum.stat.dto.ResponseDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsService {
     ResponseDto hit(RequestDto requestDto);
 
-    List<ResponseDto> stats(String start, String end, List<String> uri, boolean unique);
+    List<ResponseDto> stats(LocalDateTime start, LocalDateTime end, List<String> uri, boolean unique);
 }
