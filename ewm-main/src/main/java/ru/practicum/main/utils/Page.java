920c49f64cb5f9203bc92384fb5656ef525ced25
Sergey Malyunov <FatalRR@yandex.ru>
2023-07-07 15:43:04 +0700

    fix: change to new exception

--- ewm-main/src/main/java/ru/practicum/main/utils/Page.java
+++ ewm-main/src/main/java/ru/practicum/main/utils/Page.java
@@ -3,6 +3,8 @@ package ru.practicum.main.utils;
 import lombok.experimental.UtilityClass;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import ru.practicum.main.exception.InvalidPageParametersException;
+import ru.practicum.main.messages.ExceptionMessages;
 
 @UtilityClass
 public class Page {
@@ -10,7 +12,7 @@ public class Page {
         Pageable page;
         if (from != null && size != null) {
             if (from < 0 || size < 0) {
-                throw new RuntimeException();
+                throw new InvalidPageParametersException(ExceptionMessages.INVALID_PAGE_PARAMETERS.label);
             }
             page = PageRequest.of(from > 0 ? from / size : 0, size);
         } else {
